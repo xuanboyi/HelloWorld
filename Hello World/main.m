4d77a4ce25feb9031755c23da8516d6e73907363
玄博屹 <genhiroshi@xuanboyideMacBook-Pro.local>
2017-12-10 16:13:15 +0800

    有营养的话

--- Hello World/main.m
+++ Hello World/main.m
@@ -6,7 +6,7 @@
 //  Created by 玄博屹 on 2017/12/10.
 //  Copyright © 2017年 玄博屹. All rights reserved.
 //
-
+//   kdjflkdjglkejg;e
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
